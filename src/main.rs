72a029d82d695815267d3aed8a448a89b7551714
Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>
2024-02-15 10:31:51 +0100

    fix a regression introduced with clap v4 when using cargo scaffold as a cli
    Signed-off-by: Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 use anyhow::Result;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 use cargo_scaffold::{Opts, ScaffoldDescription};
 
+#[derive(Parser)]
+#[command(author, version, about, long_about=None)]
+struct Cli {
+    #[command(subcommand)]
+    command: ScaffoldCommand,
+}
+
+#[derive(Subcommand)]
+enum ScaffoldCommand {
+    Scaffold(Opts),
+}
+
 fn main() -> Result<()> {
-    let opts = Opts::parse();
-    ScaffoldDescription::new(opts)?.scaffold()
+    let opts = Cli::parse();
+    match opts.command {
+        ScaffoldCommand::Scaffold(opts) => ScaffoldDescription::new(opts)?.scaffold(),
+    }
 }
